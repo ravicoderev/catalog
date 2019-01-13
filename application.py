ae2de8013436985cc0f4636e3fd3ad8b935244fc
ravikumar <ravikumar.rk@gmail.com>
2019-01-13 17:06:53 +0530

    added show of items and items in category plus skeleton for edit/delete of item. html templates added with table formatting for items list

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
-from databasesetup import Base, Category
+from databasesetup import Base, Category, Item, User
 # from flask import session as login_session
 import random
 import string
@@ -26,8 +26,6 @@ def showCategories():
     return render_template('categories.html', categories=categories)
 
 # Create a new category
-
-
 @app.route('/category/new/', methods=['GET', 'POST'])
 def newCategory():
     if request.method == 'POST':
@@ -40,10 +38,7 @@ def newCategory():
         return render_template('newCategory.html')
 
 # Edit a category
-
-
 @app.route('/category/<int:category_id>/edit/', methods=['GET', 'POST'])
-# def editCategory(category_id):
 def editCategory(category_id):
     # categories = session.query(Category).order_by(asc(Category.category_name))
     print(category_id)
@@ -72,6 +67,83 @@ def deleteCategory(category_id):
     else:
         return render_template('deletecategory.html', category=deleteQuery)
 
+# Show all items
+@app.route('/items/')
+def showAllItems():
+    items = session.query(Item).order_by(asc(Item.item_name))
+    return render_template('items.html', items=items)
+
+# Show all items in Category
+@app.route('/category/<int:category_id>/')
+@app.route('/category/<int:category_id>/items/')
+def showCategoryItems(category_id):
+    category = session.query(Category).filter_by(category_id=category_id).one()
+    items = session.query(Item).filter_by(category_id = category_id).all()
+
+    return render_template('items1.html', items=items, category=category)
+
+
+# Add new item for a Category
+@app.route('/category/<int:category_id>/new', methods=['GET', 'POST'])
+def addNewItemForCategory(category_id):
+    category = session.query(Category).filter_by(category_id=category_id).one()
+    if request.method == 'POST':
+        newItem = Item(item_name=request.form['name'], item_description=request.form['description'], 
+        category_id=category_id, user_id=request.form['user_id'])
+        session.add(newItem)
+        flash('SUCCESS: New Item %s Successfully Created' % newItem.item_name)
+        session.commit()
+        return redirect(url_for('showAllItems'))
+    else:
+        return render_template('newitem.html', category=category)
+
+
+# Edit item in a Category
+# @app.route('/items/edit')
+@app.route('/category/<int:category_id>/items/<int:item_id>/edit', methods=['GET', 'POST'])
+def editItemInCategory(category_id, item_id):
+#     # editItemQuery = session.query(Item).filter_by(item_id=item_id).one()
+    
+#     # if request.method == 'POST':
+#     #     if request.form['name']:
+#     #         editItemQuery.item_name = request.form['name']
+#     #         flash('Edit Successful: %s' % editItemQuery.category_name)
+#     #         return redirect(url_for('showCategories'))
+#     # else:
+#     #     return render_template('editcategory.html', category=editQuery)
+    return("Edit item in a Category: WIP")
+
+
+# Edit item in a Category
+# @app.route('/items/edit')
+@app.route('/category/<int:category_id>/items/<int:item_id>/delete', methods=['GET', 'POST'])
+def deleteItemInCategory(category_id, item_id):
+#     # editItemQuery = session.query(Item).filter_by(item_id=item_id).one()
+    
+#     # if request.method == 'POST':
+#     #     if request.form['name']:
+#     #         editItemQuery.item_name = request.form['name']
+#     #         flash('Edit Successful: %s' % editItemQuery.category_name)
+#     #         return redirect(url_for('showCategories'))
+#     # else:
+#     #     return render_template('editcategory.html', category=editQuery)
+    return("Delete item in a Category: WIP")
+
+
+# Add new item for a Category
+# @app.route('/items/new/', methods=['GET', 'POST'])
+# def addItem():
+#     if request.method == 'POST':
+#         newItem = Item(item_name=request.form['name'])
+#         newItem = Item(item_description=request.form['description'])
+#         newItem = Item(category_id=request.form['category_id'])
+#         newItem = Item(user_id=request.form['user_id'])
+#         session.add(newItem)
+#         flash('SUCCESS: New Item %s Successfully Created' % newItem.item_name)
+#         session.commit()
+#         return redirect(url_for('showAllItems'))
+#     else:
+#         return render_template('newitem.html')
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
